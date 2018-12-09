7aacbd92677666828cc91a96ac782f1a3622022a
marcingal <marcin921125@gmail.com>
2018-12-09 23:05:54 +0100

    ready to write components

--- src/store.js
+++ src/store.js
@@ -8,6 +8,7 @@ const reducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+
 export const store = createStore(
     reducer,
     composeEnhancers(
