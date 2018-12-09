6db2eeba831f68965bcb5e61eeba8a3f96b75bfa
marcingal <marcin921125@gmail.com>
2018-12-09 17:18:36 +0100

    basics actions added

---
+++ src/state/actions.js
@@ -0,0 +1,31 @@
+import { TOGGLE_TODO, SET_VISIBILITY_FILTER } from "../actions";
+
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+export const Visibility_filters = {
+    SHOW_ALL: 'SHOW_ALL'
+}
+
+export function addTodo(text){
+    return {
+        type: ADD_TODO,
+        text
+    }
+}
+
+export function toggleTodo(index){
+    return {
+        type: TOGGLE_TODO,
+        index
+    }
+}
+
+export function setVisibilityFilter(filter){
+return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+}
+}
+
